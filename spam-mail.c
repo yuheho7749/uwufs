fb6e40f55228f9a9d358be7c809548849aeff799
Joseph <joseph_ng@ucsb.edu>
2024-10-15 17:23:13 -0700

    [TEST] In memory fake file system

--- spam-mail.c
+++ spam-mail.c
@@ -19,14 +19,35 @@
 #include <errno.h>
 #include <string.h>
 
-
-static struct fake_file {
-	const char *filename;
-	const char *contents;
-} fake_file;
+#define MAX_FILES 3 // TEST:
 
 
-static void* spam_mail_init(struct fuse_conn_info *conn, struct fuse_config *cfg) {
+// TEMP: Phase 1: Stores the files in memory 
+static struct fake_filesystem {
+	char *file_names[MAX_FILES];
+	char *file_contents[MAX_FILES];
+	int no_files;
+} fake_filesystem;
+
+// TEMP: Helper to search for files in our fake filesystem
+static int search_file(const char *path) {
+
+	printf("searching for file: %s\n", path);
+	int i;
+	char *f_name;
+	for (i = 0; i < fake_filesystem.no_files; i++) {
+		f_name = fake_filesystem.file_names[i];
+		if (strcmp(path, f_name) == 0) {
+			printf("found file: %d\n", i);
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+static void* spam_mail_init(struct fuse_conn_info *conn,
+							struct fuse_config *cfg) {
 	(void) conn;
 	cfg->direct_io = 1; 	// disables page caching in the kernel
 	return NULL;
@@ -40,20 +61,27 @@ static void* spam_mail_init(struct fuse_conn_info *conn, struct fuse_config *cfg
  * 	will read the raw bytes from our fs and set the correct output values
  * 	in stbuf
  */
-static int spam_mail_getattr(const char *path, struct stat *stbuf, struct fuse_file_info *fi) {
+static int spam_mail_getattr(const char *path, struct stat *stbuf, 
+							 struct fuse_file_info *fi) {
 	(void) fi;	
 
 	memset(stbuf, 0, sizeof(struct stat));
 	if (strcmp(path, "/") == 0) {
 		stbuf->st_mode = S_IFDIR | 0755;
-		stbuf->st_nlink = 2; 	// A directory has at least 2 hard links because "." 
-								// inside itself also refers to itself
-	} else if (strcmp(path + 1, fake_file.filename) == 0) { 
-		// path + 1 because the filepath is "/filename.txt" and we don't want the "/"
+		stbuf->st_nlink = 2;
+		// A directory has at least 2 hard links because "." 
+		// inside itself also refers to itself
+		return 0;
+	} 
+
+	int file_id;
+	file_id = search_file(path);
+	if (file_id >= 0) { 
+		// TEMP: Hard coding the file information (except for the contents)
 		stbuf->st_mode = S_IFREG | 0644;
 		stbuf->st_nlink = 1;
 
-		stbuf->st_size = strlen(fake_file.contents);
+		stbuf->st_size = strlen(fake_filesystem.file_contents[file_id]);
 		// assuming block size of 4k bytes (4096 bytes)
 		stbuf->st_blocks = (stbuf->st_size + 4095) / 4096;
 
@@ -63,23 +91,23 @@ static int spam_mail_getattr(const char *path, struct stat *stbuf, struct fuse_f
 		stbuf->st_atime = time(NULL);
 		stbuf->st_mtime = stbuf->st_atime;
 		stbuf->st_ctime = stbuf->st_atime;
-	} else {
-		// TODO: This is all hardcoded, might want to actually allow
-		// user to make their own files
-		return -ENOENT;
+		return 0;
 	}
 
-	return 0;
+	return -ENOENT;
 }
 
 /**
  *	Similar to getattr, but for directories
  */
-static int spam_mail_readdir(const char *path, void *buf, fuse_fill_dir_t filler, 
-							 off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags) {
+static int spam_mail_readdir(const char *path, void *buf, 
+							 fuse_fill_dir_t filler, off_t offset, 
+							 struct fuse_file_info *fi, 
+							 enum fuse_readdir_flags flags) {
 	(void) offset;
 	(void) fi;
 	(void) flags;
+
 
 	if (strcmp(path, "/") != 0) {
 		return -ENOENT;
@@ -87,7 +115,11 @@ static int spam_mail_readdir(const char *path, void *buf, fuse_fill_dir_t filler
 	
 	filler(buf, ".", NULL, 0, FUSE_FILL_DIR_DEFAULTS);
 	filler(buf, "..", NULL, 0, FUSE_FILL_DIR_DEFAULTS);
-	filler(buf, fake_file.filename, NULL, 0, FUSE_FILL_DIR_DEFAULTS);
+
+	int i;
+	for (i = 0; i < fake_filesystem.no_files; i++) {
+		filler(buf, fake_filesystem.file_names[i] + 1, NULL, 0, FUSE_FILL_DIR_DEFAULTS);
+	}
 
 	return 0;
 }
@@ -101,42 +133,63 @@ static int spam_mail_readdir(const char *path, void *buf, fuse_fill_dir_t filler
  *	
  *	Phase 2 Requirements: We need to create this file in storage
  */
-static int spam_mail_create(const char *path, mode_t mode, struct fuse_file_info *fi) {
-	// TODO:	
+static int spam_mail_create(const char *path, mode_t mode, 
+							struct fuse_file_info *fi) {
+	(void) fi;
 
+	// TEST:
+	printf("create file path: %s\n", path);
 
+	// // NOTE: Storing it in our in memory fake_filesystem
+	// if (fake_filesystem.no_files >= MAX_FILES) {
+	// 	return -ENOSPC;
+	// }
+
+	// TEMP: Ignore the file create mode for now
+	// int file_id;
+	// file_id = fake_filesystem.no_files;
+	// fake_filesystem.file_names[file_id] = strdup(path);
+	// fake_filesystem.file_contents[file_id] = strdup("");
+	//
 	return 0;
 }
 
 static int spam_mail_open(const char *path, struct fuse_file_info *fi) {
+	int ret;
 
-	// TEMP: Only care about our fake file
-	if (strcmp(path + 1, fake_file.filename) != 0) {
-		return -ENOENT;
+	printf("attempting to open: %s\n", path);
+	int file_id;
+	file_id = search_file(path);  // TEMP: Using fake filesystem
+	if (file_id < 0) {
+		ret = -ENOENT;
 	}
 
-	// TODO: Check the file permissions vs the open file permission in fi
 	// if ((fi->flags & O_ACCMODE) != O_RDONLY)
 	// 	return -EACCES;
 
-	return 0;
+	return ret;
 }
 
-static int spam_mail_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
+static int spam_mail_read(const char *path, char *buf, size_t size, 
+						  off_t offset, struct fuse_file_info *fi) {
 	(void) fi;
 
-	// TEMP: Only care about our fake file
-	if(strcmp(path + 1, fake_file.filename) != 0) {
+	printf("attempting to read: %s\n", path);
+	int file_id;
+	file_id = search_file(path);  // TEMP: Using fake filesystem
+	if (file_id < 0) {
 		return -ENOENT;
 	}
 
 	int len;
-	len = strlen(fake_file.contents);
+	len = strlen(fake_filesystem.file_contents[file_id]);
+	printf("file content len: %d\n", len);
+	printf("file content: %s\n", fake_filesystem.file_contents[file_id]);
 	if (offset < len) {
 		if (offset + size > len) {
 			size = len - offset;
 		}
-		memcpy(buf, fake_file.contents + offset, size);
+		memcpy(buf, fake_filesystem.file_contents[file_id] + offset, size);
 	} else {
 		size = 0;
 	}
@@ -144,13 +197,15 @@ static int spam_mail_read(const char *path, char *buf, size_t size, off_t offset
 	return size;
 }
 
-static int spam_mail_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
+static int spam_mail_write(const char *path, const char *buf, size_t size, 
+						   off_t offset, struct fuse_file_info *fi) {
 	
 	(void) path;
 	(void) fi;
 
-	// TEMP: Only care about our fake file
-	if(strcmp(path + 1, fake_file.filename) != 0) {
+	int file_id;
+	file_id = search_file(path);  // TEMP: Using fake filesystem
+	if (file_id < 0) {
 		return -ENOENT;
 	}
 
@@ -169,12 +224,12 @@ static int spam_mail_write(const char *path, const char *buf, size_t size, off_t
 }
 
 static const struct fuse_operations spam_mail_oper = {
-	.init       = spam_mail_init, 		// technically not needed, but I'm enabling direct_io to avoid page caching
+	.init       = spam_mail_init,
 	.getattr	= spam_mail_getattr,
 	.readdir	= spam_mail_readdir,
-	.create 	= spam_mail_create,		// TODO:
+	.create 	= spam_mail_create,
 	.open		= spam_mail_open,
-	.read		= spam_mail_read, 		// technically not needed for phase 1
+	.read		= spam_mail_read,
 	.write		= spam_mail_write,
 };
 
@@ -196,7 +251,8 @@ int main(int argc, char *argv[]) {
 	}
 
 	if (stat(opts.mountpoint, &stbuf) == -1) {
-		fprintf(stderr ,"failed to access mountpoint %s: %s\n", opts.mountpoint, strerror(errno));
+		fprintf(stderr ,"failed to access mountpoint %s: %s\n", 
+		 		opts.mountpoint, strerror(errno));
 		free(opts.mountpoint);
 		return 1;
 	}
@@ -208,11 +264,12 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	// Add a fake file inside the mounted fs to make sure the mounting is correct
-	fake_file.filename = strdup("README.txt");
-	fake_file.contents = strdup("Writing to any file in this mounted directory will send spam mail!\n");
+	// Add a fake file inside in our fake filesystem
+	fake_filesystem.file_names[0] = strdup("/README.txt");
+	fake_filesystem.file_contents[0] = strdup("Writing to any file in this mounted directory will send spam mail!\n");
+	fake_filesystem.no_files = 1;
 
 	ret = fuse_main(argc, argv, &spam_mail_oper, NULL);
 
-	return 0;
+	return ret;
 }
