939fd82e0ebba0f7ab5a81b3e8128e065b938b33
Joseph <joseph_ng@ucsb.edu>
2024-11-23 00:38:02 -0800

    [FEAT] testing for mass mkdir/rmdir/touch/rm

---
+++ src/test/test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cd /mnt/uwufs
+
+# for i in {1..1000}
+# do
+#   mkdir "dir$i"
+# done
+#
+# for dir in */
+# do
+#   # Check if it's a directory
+#   if [ -d "$dir" ]; then
+#     rmdir "$dir"
+#     echo "Removed: $dir"
+#   fi
+# done
+
+for i in {1..1000}
+do
+  touch "file$i.txt"
+done
